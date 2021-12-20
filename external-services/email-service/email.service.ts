0b28633c2d6c3a1d2025e19c5b7fc876cc020187
charl890 <40757189+charl890@users.noreply.github.com>
2021-12-20 13:39:35 +0800

    Add files via upload

--- external-services/email-service/email.service.ts
+++ external-services/email-service/email.service.ts
@@ -5,17 +5,17 @@ class EmailService implements IEmailService {
     async sendEmail(resource: EmailDto) {
      // Sending and receiving data in JSON format using POST method
      //
-     var xhr = new XMLHttpRequest();
-     var url = "http://localhost:9100/ExtEmailService";
+     const xhr = new XMLHttpRequest();
+     const url = "http://localhost:9100/ExtEmailService";
      xhr.open("POST", url, true);
      xhr.setRequestHeader("Content-Type", "application/json");
      xhr.onreadystatechange = function () {
        if (xhr.readyState === 4 && xhr.status === 200) {
-        var json = JSON.parse(xhr.responseText);
+        const json = JSON.parse(xhr.responseText);
         console.log(json.email);
       }
      };
-     var data = JSON.stringify({"ToEmail": resource.sendTo, "FromEmail": resource.sendFrom, "Subject": resource.subject, "Body": resource.body});
+     const data = JSON.stringify({"ToEmail": resource.sendTo, "FromEmail": resource.sendFrom, "Subject": resource.subject, "Body": resource.body});
      xhr.send(data);
      return (resource);
     }
